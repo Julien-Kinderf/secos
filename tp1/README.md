5631db141a49080f69e847c70eb01481168fec06
kinderf <kinderf@etud.insa-toulouse.fr>
2020-11-28 10:59:53 +0100

    TP1-1

--- tp1/README.md
+++ tp1/README.md
@@ -28,7 +28,7 @@ par
 **Grub a démarré notre noyau en mode protégé. Il a donc configuré une GDT avant d'exécuter notre point d'entrée. Affichez le contenu de cette GDT. Que constatez-vous ?**
 
 **Servez-vous des outils présents dans notre OS (`get_gdtr(), seg_desc_t et gdt_reg_t`)**
-
+> On constate qu'il y a déjà 5 segments de définis, qui commencent tous à 0, ont tous une limite de 65520 et un type de 0.
 ---
 
 ### Question 2
--- tp1/tp.c
+++ tp1/tp.c
@@ -1,9 +1,30 @@
 /* GPLv2 (c) Airbus */
 #include <debug.h>
 #include <info.h>
+#include <segmem.h>
 
 extern info_t *info;
 
+void print_gdt()
+{
+    gdt_reg_t default_gdtr;
+    get_gdtr(default_gdtr);
+    printf("Default gdt is located at %p and its limit is %d\n", default_gdtr.addr, default_gdtr.limit);
+    printf("Displaying the default gdt ...\n");
+
+    seg_desc_t *current_seg_desc;
+    for (unsigned int entry = 0; entry < (default_gdtr.limit + 1) / sizeof(seg_desc_t); entry++)
+    {
+        current_seg_desc = default_gdtr.desc;
+        int base = (current_seg_desc->base_3 << 24) | (current_seg_desc->base_2 << 16) | current_seg_desc->base_2;
+        int limit = (current_seg_desc->limit_2 << 16) | current_seg_desc->limit_1;
+        printf("Segment %d : base = %p, limit = %d, type=%d\n", entry, base, limit, current_seg_desc->type);
+        current_seg_desc++;
+    }
+}
+
 void tp()
 {
+    // Question 1 : afficher la gdt :
+    print_gdt();
 }
